bfd067a8dc1b9b89dd93afb7ca66aa85ad19b164
ethmth <ethan@smaze.net>
2024-04-22 21:58:13 -0400

    Add_item function works correctly

--- project/scripts/test/test_script.gd
+++ project/scripts/test/test_script.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var time_passed: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +9,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(Items.items)
+	time_passed += delta
+	if(time_passed > 1.0):
+		print(Items.items)
+		print(Items.player_items)
+		time_passed = 0.0
+	Items.add_item("wood")
 	pass
