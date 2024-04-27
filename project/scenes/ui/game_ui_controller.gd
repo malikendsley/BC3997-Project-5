b9f90dbb15420e03d1f1cf6318184facc1de4222
ethmth <ethan@smaze.net>
2024-04-26 23:23:57 -0400

    Split inventory item into its own scene

--- project/scenes/ui/game_ui_controller.gd
+++ project/scenes/ui/game_ui_controller.gd
@@ -12,28 +12,28 @@ var hp: int = 0
 var energy: int = 0
 
 func initialize(max_health, maximum_energy):
-    print("Initializing Game UI: max_health: ", max_health, " maximum_energy: ", maximum_energy)
-    max_hp = max_health
-    max_energy = maximum_energy
-    hp_progress_bar.max_value = max_hp
-    energy_progress_bar.max_value = max_energy
-    set_hp(max_hp)
-    set_energy(max_energy)
+	print("Initializing Game UI: max_health: ", max_health, " maximum_energy: ", maximum_energy)
+	max_hp = max_health
+	max_energy = maximum_energy
+	hp_progress_bar.max_value = max_hp
+	energy_progress_bar.max_value = max_energy
+	set_hp(max_hp)
+	set_energy(max_energy)
 
 func set_hp(new_hp: float) -> void:
-    hp_progress_bar.value = new_hp
-    if new_hp < 0:
-        new_hp = 0
-    icon.frame = int(lerp(5, 0, new_hp / max_hp))
+	hp_progress_bar.value = new_hp
+	if new_hp < 0:
+		new_hp = 0
+	icon.frame = int(lerp(5, 0, new_hp / max_hp))
 
 func set_energy(new_energy: float) -> void:
-    energy_progress_bar.value = new_energy
+	energy_progress_bar.value = new_energy
 
 # TODO: For testing, called by a button, remove later
 func _damage_all_enemies():
-    print("damaging all enemies")
-    # get all entities and hit them with player damage, won't hurt the player
-    for enemy in get_tree().get_nodes_in_group("entities"):
-        print("Entity: ", enemy.name)
-        var damage_instance = DamageInstance.new(1, DamageInstance.Faction.PLAYER, self, Vector2(0, 0))
-        enemy.hurt_component.send_damage(damage_instance)
+	print("damaging all enemies")
+	# get all entities and hit them with player damage, won't hurt the player
+	for enemy in get_tree().get_nodes_in_group("entities"):
+		print("Entity: ", enemy.name)
+		var damage_instance = DamageInstance.new(1, DamageInstance.Faction.PLAYER, self, Vector2(0, 0))
+		enemy.hurt_component.send_damage(damage_instance)
