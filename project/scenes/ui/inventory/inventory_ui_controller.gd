e3b75785e792285937f027e82e4bf99a0ea3f95e
ethmth <ethan@smaze.net>
2024-04-27 00:34:13 -0400

    Dynamically load inventory items

--- project/scenes/ui/inventory/inventory_ui_controller.gd
+++ project/scenes/ui/inventory/inventory_ui_controller.gd
@@ -3,8 +3,46 @@ extends Node
 var time_since_change : float = 0
 const BUTTON_DELAY: float = 0.5
 
+var inventory_items_container: BoxContainer = null
+var item_scene = preload("res://project/scenes/ui/inventory/inventory_item_ui.tscn")
+var inventory:Node2D = null
+
+func get_inventory_node():
+	var node_list = get_tree().get_nodes_in_group("player")
+	if len(node_list) <= 0 or len(node_list) > 1:
+		inventory = null
+		print("inventory_ui_controller.gd error: Unexpected (non-1) number of nodes in group 'player'")
+		return
+	var player_node:CharacterBody2D = node_list[0]
+	inventory = player_node.get_node("Inventory")
+
 func _ready():
 	self.visible = false
+	inventory_items_container = get_node("InventoryControl/ScrollContainer/InventoryItemsContainer")
+	get_inventory_node()
+
+func add_items_node(item:String, quantity: int):
+	var item_node_instance = item_scene.instantiate()
+	item_node_instance.initialize(item, quantity)
+	inventory_items_container.add_child(item_node_instance)
+
+func gen_items_list():
+	for item_id in inventory.items:
+		if not inventory.has_item(item_id):
+			continue
+
+		var quantity: int = inventory.get_quantity(item_id)
+		add_items_node(item_id, quantity)
+
+func remove_children(parent):
+	for n in parent.get_children():
+		parent.remove_child(n)
+		n.queue_free()
+
+func refresh_inventory():
+	remove_children(inventory_items_container)
+	gen_items_list()
+	pass
 
 func _process(delta:float):
 	time_since_change += delta
@@ -22,10 +60,11 @@ func _process(delta:float):
 			else:
 				time_since_change = 0.0
 				open_inventory_screen()
-		
+
 func close_inventory_screen():
 	self.visible = false
-	
+
 func open_inventory_screen():
+	refresh_inventory()
 	self.visible = true
 
