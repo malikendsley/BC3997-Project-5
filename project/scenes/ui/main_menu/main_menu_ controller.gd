51e13abf832154f7bb76b084e3ab2f2eaa0f5048
ethmth <ethan@smaze.net>
2024-05-03 23:49:22 -0400

    Fixed inventory ui sizing

--- project/scenes/ui/main_menu/main_menu_ controller.gd
+++ project/scenes/ui/main_menu/main_menu_ controller.gd
@@ -12,21 +12,21 @@ extends Control
 @onready var main_menu: Control = %MainMenuControl
 
 func _ready():
-    start_button.pressed.connect(start_game)
-    settings_button.pressed.connect(settings_menu_pressed)
-    quit_button.pressed.connect(quit_game)
-    
+	start_button.pressed.connect(start_game)
+	settings_button.pressed.connect(settings_menu_pressed)
+	quit_button.pressed.connect(quit_game)
+	
 func start_game():
-    print("Start game")
-    get_tree().change_scene_to_file(game_scene_name)
+	print("Start game")
+	get_tree().change_scene_to_file(game_scene_name)
 
 func settings_menu_pressed():
-    print("Settings menu pressed")
-    swap_menus()
+	print("Settings menu pressed")
+	swap_menus()
 
 func quit_game():
-    get_tree().quit()
+	get_tree().quit()
 
 func swap_menus():
-    main_menu.visible = !main_menu.visible
-    settings_menu.visible = !settings_menu.visible
+	main_menu.visible = !main_menu.visible
+	settings_menu.visible = !settings_menu.visible
