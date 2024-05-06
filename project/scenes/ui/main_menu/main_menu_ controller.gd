35f1eddf0e0af642b687703771bf17a4f0e19fe5
Malik Endsley <malik.s.endsley@gmail.com>
2024-05-06 03:15:17 -0400

    fully functional (artless) main menu + settings scene + settings menu

--- project/scenes/ui/main_menu/main_menu_ controller.gd
+++ project/scenes/ui/main_menu/main_menu_ controller.gd
@@ -1,9 +1,10 @@
 extends Control
+class_name MainMenu
 
 @onready var start_button: Button = %StartButton
 @onready var settings_button: Button = %SettingsButton
 @onready var quit_button: Button = %QuitButton
-
+@onready var tab_container: TabContainer = %TabContainer
 # TODO: Update if needed
 @export var game_scene_name = "res://project/game.tscn"
 
@@ -11,22 +12,32 @@ extends Control
 @onready var settings_menu: Control = %SettingsControl
 @onready var main_menu: Control = %MainMenuControl
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel") and settings_menu.visible:
+		settings_menu.visible = false
+		main_menu.visible = true
+
 func _ready():
+	main_menu.visible = true
+	settings_menu.visible = false
+	tab_container.current_tab = 0
 	start_button.pressed.connect(start_game)
-	settings_button.pressed.connect(settings_menu_pressed)
+	settings_button.pressed.connect(settings_opened)
 	quit_button.pressed.connect(quit_game)
 	
 func start_game():
 	print("Start game")
 	get_tree().change_scene_to_file(game_scene_name)
 
-func settings_menu_pressed():
+func settings_opened():
 	print("Settings menu pressed")
-	swap_menus()
+	main_menu.visible = false
+	settings_menu.visible = true
+
+func back_to_main_menu():
+	print("Back to main menu")
+	main_menu.visible = true
+	settings_menu.visible = false
 
 func quit_game():
 	get_tree().quit()
-
-func swap_menus():
-	main_menu.visible = !main_menu.visible
-	settings_menu.visible = !settings_menu.visible
